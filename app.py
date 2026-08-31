42aead2ca28d8a528c6cfb9fea988c219ad8331a
kariyawasambgmsri-star <kariyawasambgmsri@gmail.com>
2026-08-31 22:14:35 +0530

    Add customer churn prediction notebook

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import joblib
+import numpy as np 
+
+scaler = joblib.load("Scaler.pkl")
+model = joblib.load("model.pkl")
+
+st.title("Churn Prediction App")
+
+st.divider()
+
+st.write("Please enter the value and hit the predict button for getting a prediction.")
+
+st.divider()
+
+age = st.number_input("Enter age", min_value=10, max_value=100, value=30)
+
+tenure = st.number_input("Enter Tenure", min_value=0, max_value=130, value= 10)
+
+monthlycharge = st.number_input("Enter Monthly Charge",min_value=30,max_value=150)
+
+gender = st.selectbox("Enter the Gender",["Male","Female"])
+
+st.divider()
+
+prediction = st.button("predict!")
+
+if prediction:
+    gender_selected = 1 if gender == "Female" else 0
+    x = [age, gender_selected, tenure, monthlycharge]
+    x1 = np.array(x)
+    x_array = scaler.transform([x1])
+    prediction = model.predict(x_array)[0]
+    predicted = "Yes" if prediction == 1 else "No"
+    st.write(f"predicted: {predicted}")
+
+else: 
+    st.write("please enter the values and use predict button")
+
